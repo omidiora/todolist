488468fa140e397b60be5f2b5ac4bb5f6ece80b1
omidiora <omidioraemmanuel@gmail.com>
2020-03-02 22:56:11 +0100

    second commit

---
+++ todoapp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import  Employee
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model=Employee
+        fields= ('fullname','mobile','emp_code','position')
+    
+        def __init__(self,*args,**kwargs):
+            super(Employee,self).__init__(*args,**kwargs)
+            self.fields['position'].empty_label="Select"
+    
+ 
